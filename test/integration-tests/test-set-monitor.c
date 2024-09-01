ebb581c2200b8adc3436cdebc5bb8090810383a0
Sophie Winter <git@phie.me>
2024-09-01 16:02:22 -0700

    Make tests compatible with new libwayland format

--- test/integration-tests/test-set-monitor.c
+++ test/integration-tests/test-set-monitor.c
@@ -4,7 +4,7 @@ static GtkWindow* window;
 
 static void callback_0()
 {
-    EXPECT_MESSAGE(zwlr_layer_shell_v1 .get_layer_surface wl_output@);
+    EXPECT_MESSAGE(zwlr_layer_shell_v1 .get_layer_surface wl_output);
     window = create_default_window();
     gtk_layer_init_for_window(window);
     ASSERT_EQ(g_list_model_get_n_items(gdk_display_get_monitors(gdk_display_get_default())), 1, "%d");
