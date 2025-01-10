fe11c12635f13c3ca135aa1d0143ae165434797f
Sophie Winter <git@phie.me>
2025-01-09 21:53:04 -0800

    Remove unused wayland-utils functions

--- src/wayland-utils.h
+++ src/wayland-utils.h
@@ -8,7 +8,3 @@
 
 void gtk_wayland_init_if_needed();
 struct zwlr_layer_shell_v1* gtk_wayland_get_layer_shell_global();
-
-enum xdg_positioner_gravity gdk_gravity_get_xdg_positioner_gravity(GdkGravity gravity);
-enum xdg_positioner_anchor gdk_gravity_get_xdg_positioner_anchor(GdkGravity anchor);
-enum xdg_positioner_constraint_adjustment gdk_anchor_hints_get_xdg_positioner_constraint_adjustment(GdkAnchorHints hints);
