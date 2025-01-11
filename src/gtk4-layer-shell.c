197f0424d1a9ef1e58a0be5a29eee3afec317d08
Sophie Winter <git@phie.me>
2025-01-11 03:43:31 -0800

    Rename type LayerSurface -> struct gtk_layer_surface_t

--- src/gtk4-layer-shell.c
+++ src/gtk4-layer-shell.c
@@ -8,11 +8,11 @@
 static const char* layer_surface_key = "wayland_layer_surface";
 static GList* all_layer_surfaces = NULL;
 
-typedef struct {
+struct gtk_layer_surface_t {
     struct layer_surface_t super;
     GtkWindow* gtk_window;
     GdkMonitor* monitor;
-} LayerSurface;
+};
 
 guint gtk_layer_get_major_version() {
     return GTK_LAYER_SHELL_MAJOR;
@@ -47,14 +47,14 @@ guint gtk_layer_get_protocol_version() {
     return zwlr_layer_shell_v1_get_version(layer_shell_global);
 }
 
-static LayerSurface* gtk_window_get_layer_surface(GtkWindow* gtk_window) {
+static struct gtk_layer_surface_t* gtk_window_get_layer_surface(GtkWindow* gtk_window) {
     if (!gtk_window) return NULL;
     return g_object_get_data(G_OBJECT(gtk_window), layer_surface_key);
 }
 
-static LayerSurface* gtk_window_get_layer_surface_or_warn(GtkWindow* window) {
+static struct gtk_layer_surface_t* gtk_window_get_layer_surface_or_warn(GtkWindow* window) {
     g_return_val_if_fail(window, NULL);
-    LayerSurface* layer_surface = gtk_window_get_layer_surface(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface(window);
     if (!layer_surface) {
         g_warning("GtkWindow is not a layer surface. Make sure you called gtk_layer_init_for_window()");
         return NULL;
@@ -62,14 +62,14 @@ static LayerSurface* gtk_window_get_layer_surface_or_warn(GtkWindow* window) {
     return layer_surface;
 }
 
-static void layer_surface_destroy(LayerSurface* self) {
+static void gtk_layer_surface_destroy(struct gtk_layer_surface_t* self) {
     layer_surface_uninit(&self->super);
     all_layer_surfaces = g_list_remove(all_layer_surfaces, self);
     g_free(self);
 }
 
 static gint find_layer_surface_with_wl_surface(gconstpointer layer_surface, gconstpointer needle) {
-    LayerSurface* self = (LayerSurface*)layer_surface;
+    struct gtk_layer_surface_t* self = (struct gtk_layer_surface_t*)layer_surface;
     GdkSurface* gdk_surface = gtk_native_get_surface(GTK_NATIVE(self->gtk_window));
     if (!gdk_surface) return 1;
     struct wl_surface* wl_surface = gdk_wayland_surface_get_wl_surface(gdk_surface);
@@ -85,8 +85,8 @@ static struct layer_surface_t* get_layer_surface_for_wl_surface_impl(struct wl_s
     return layer_surface_entry ? layer_surface_entry->data : NULL;
 }
 
-static struct geom_size_t layer_surface_get_preferred_size_impl(struct layer_surface_t* super) {
-    LayerSurface* self = (LayerSurface*)super;
+static struct geom_size_t gtk_layer_surface_get_preferred_size(struct layer_surface_t* super) {
+    struct gtk_layer_surface_t* self = (struct gtk_layer_surface_t*)super;
     struct geom_size_t size = {0};
     gtk_window_get_default_size(self->gtk_window, &size.width, &size.height);
 
@@ -99,18 +99,18 @@ static struct geom_size_t layer_surface_get_preferred_size_impl(struct layer_sur
     return size;
 }
 
-static void layer_surface_on_default_size_set(
+static void gtk_layer_surface_on_default_size_set(
     GtkWindow* _window,
     const GParamSpec* _pspec,
     struct layer_surface_t* layer_surface
 ) {
     (void)_window;
     (void)_pspec;
-    layer_surface_invalidate_preferred_size(&((LayerSurface*)layer_surface)->super);
+    layer_surface_invalidate_preferred_size(&((struct gtk_layer_surface_t*)layer_surface)->super);
 }
 
-static void layer_surface_remap_impl(struct layer_surface_t* super) {
-    LayerSurface* self = (LayerSurface*)super;
+static void gtk_layer_surface_remap(struct layer_surface_t* super) {
+    struct gtk_layer_surface_t* self = (struct gtk_layer_surface_t*)super;
     gtk_widget_unrealize(GTK_WIDGET(self->gtk_window));
     gtk_widget_map(GTK_WIDGET(self->gtk_window));
 }
@@ -141,18 +141,18 @@ void gtk_layer_init_for_window(GtkWindow* window) {
 
     get_layer_surface_for_wl_surface = get_layer_surface_for_wl_surface_impl;
 
-    LayerSurface* layer_surface = g_new0(LayerSurface, 1);
+    struct gtk_layer_surface_t* layer_surface = g_new0(struct gtk_layer_surface_t, 1);
     layer_surface->gtk_window = window;
     layer_surface->super = layer_surface_make();
-    layer_surface->super.remap = layer_surface_remap_impl;
-    layer_surface->super.get_preferred_size = layer_surface_get_preferred_size_impl;
-    g_object_set_data_full(G_OBJECT(window), layer_surface_key, layer_surface, (GDestroyNotify)layer_surface_destroy);
+    layer_surface->super.remap = gtk_layer_surface_remap;
+    layer_surface->super.get_preferred_size = gtk_layer_surface_get_preferred_size;
+    g_object_set_data_full(G_OBJECT(window), layer_surface_key, layer_surface, (GDestroyNotify)gtk_layer_surface_destroy);
 
     all_layer_surfaces = g_list_append(all_layer_surfaces, layer_surface);
 
     gtk_window_set_decorated(window, FALSE);
-    g_signal_connect(window, "notify::default-width", G_CALLBACK(layer_surface_on_default_size_set), layer_surface);
-    g_signal_connect(window, "notify::default-height", G_CALLBACK(layer_surface_on_default_size_set), layer_surface);
+    g_signal_connect(window, "notify::default-width",  G_CALLBACK(gtk_layer_surface_on_default_size_set), layer_surface);
+    g_signal_connect(window, "notify::default-height", G_CALLBACK(gtk_layer_surface_on_default_size_set), layer_surface);
 
     if (gtk_widget_get_mapped(GTK_WIDGET(window))) {
         layer_surface->super.remap(&layer_surface->super);
@@ -164,39 +164,39 @@ gboolean gtk_layer_is_layer_window(GtkWindow* window) {
 }
 
 struct zwlr_layer_surface_v1* gtk_layer_get_zwlr_layer_surface_v1(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return NULL;
     return layer_surface->super.layer_surface;
 }
 
 void gtk_layer_set_namespace(GtkWindow* window, char const* name_space) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     layer_surface_set_name_space(&layer_surface->super, name_space);
 }
 
 const char* gtk_layer_get_namespace(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     return layer_surface_get_namespace(&layer_surface->super); // NULL-safe
 }
 
 void gtk_layer_set_layer(GtkWindow* window, GtkLayerShellLayer layer) {
     g_return_if_fail(layer >= 0 && layer < GTK_LAYER_SHELL_LAYER_ENTRY_NUMBER);
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     // Our keyboard interactivity enum matches the layer shell one
     layer_surface_set_layer(&layer_surface->super, (enum zwlr_layer_shell_v1_layer)layer);
 }
 
 GtkLayerShellLayer gtk_layer_get_layer(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return GTK_LAYER_SHELL_LAYER_TOP;
     // Our keyboard interactivity enum matches the layer shell one
     return (GtkLayerShellLayer)layer_surface->super.layer;
 }
 
 void gtk_layer_set_monitor(GtkWindow* window, GdkMonitor* monitor) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     struct wl_output* output = NULL;
     if (monitor) {
@@ -209,13 +209,13 @@ void gtk_layer_set_monitor(GtkWindow* window, GdkMonitor* monitor) {
 }
 
 GdkMonitor* gtk_layer_get_monitor(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return NULL;
     return layer_surface->monitor;
 }
 
 void gtk_layer_set_anchor(GtkWindow* window, GtkLayerShellEdge edge, gboolean anchor_to_edge) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     struct geom_edges_t anchored = layer_surface->super.anchored;
     switch (edge) {
@@ -229,7 +229,7 @@ void gtk_layer_set_anchor(GtkWindow* window, GtkLayerShellEdge edge, gboolean an
 }
 
 gboolean gtk_layer_get_anchor(GtkWindow* window, GtkLayerShellEdge edge) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return FALSE;
     switch (edge) {
         case GTK_LAYER_SHELL_EDGE_LEFT:   return layer_surface->super.anchored.left;
@@ -241,7 +241,7 @@ gboolean gtk_layer_get_anchor(GtkWindow* window, GtkLayerShellEdge edge) {
 }
 
 void gtk_layer_set_margin(GtkWindow* window, GtkLayerShellEdge edge, int margin_size) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     struct geom_edges_t margins = layer_surface->super.margin_size;
     switch (edge) {
@@ -255,7 +255,7 @@ void gtk_layer_set_margin(GtkWindow* window, GtkLayerShellEdge edge, int margin_
 }
 
 int gtk_layer_get_margin(GtkWindow* window, GtkLayerShellEdge edge) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return 0;
     switch (edge) {
         case GTK_LAYER_SHELL_EDGE_LEFT:   return layer_surface->super.margin_size.left;
@@ -267,39 +267,39 @@ int gtk_layer_get_margin(GtkWindow* window, GtkLayerShellEdge edge) {
 }
 
 void gtk_layer_set_exclusive_zone(GtkWindow* window, int exclusive_zone) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     layer_surface_set_exclusive_zone(&layer_surface->super, exclusive_zone);
 }
 
 int gtk_layer_get_exclusive_zone(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return 0;
     return layer_surface->super.exclusive_zone;
 }
 
 void gtk_layer_auto_exclusive_zone_enable(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     layer_surface_auto_exclusive_zone_enable(&layer_surface->super);
 }
 
 gboolean gtk_layer_auto_exclusive_zone_is_enabled(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return FALSE;
     return layer_surface->super.auto_exclusive_zone;
 }
 
 void gtk_layer_set_keyboard_mode(GtkWindow* window, GtkLayerShellKeyboardMode mode) {
     g_return_if_fail(mode >= 0 && mode < GTK_LAYER_SHELL_KEYBOARD_MODE_ENTRY_NUMBER);
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return;
     // Our keyboard interactivity enum matches the layer shell one
     layer_surface_set_keyboard_mode(&layer_surface->super, (enum zwlr_layer_surface_v1_keyboard_interactivity)mode);
 }
 
 GtkLayerShellKeyboardMode gtk_layer_get_keyboard_mode(GtkWindow* window) {
-    LayerSurface* layer_surface = gtk_window_get_layer_surface_or_warn(window);
+    struct gtk_layer_surface_t* layer_surface = gtk_window_get_layer_surface_or_warn(window);
     if (!layer_surface) return GTK_LAYER_SHELL_KEYBOARD_MODE_NONE;
     // Our keyboard interactivity enum matches the layer shell one
     return (GtkLayerShellKeyboardMode)layer_surface->super.keyboard_mode;
