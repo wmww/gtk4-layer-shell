517484abe51ca59405af5ba3b0e1e12d271a6595
Sophie Winter <git@phie.me>
2025-01-26 06:10:50 -0800

    Add basic session lock support to mock server

--- test/mock-server/overrides.c
+++ test/mock-server/overrides.c
@@ -6,6 +6,7 @@ typedef enum {
     SURFACE_ROLE_XDG_TOPLEVEL,
     SURFACE_ROLE_XDG_POPUP,
     SURFACE_ROLE_LAYER,
+    SURFACE_ROLE_SESSION_LOCK,
 } SurfaceRole;
 
 typedef struct SurfaceData SurfaceData;
@@ -20,12 +21,15 @@ struct SurfaceData {
     struct wl_resource* xdg_popup;
     struct wl_resource* xdg_surface;
     struct wl_resource* layer_surface;
+    struct wl_resource* lock_surface;
     char has_committed_buffer; // This surface has a non-null committed buffer
     char initial_commit_for_role; // Set to 1 when a role is created for a surface, and cleared after the first commit
     char layer_send_configure; // If to send a layer surface configure on the next commit
     int layer_set_w; // The width to configure the layer surface with
     int layer_set_h; // The height to configure the layer surface with
     uint32_t layer_anchor; // The layer surface's anchor
+    uint32_t lock_surface_pending_serial;
+    char lock_surface_initial_configure_acked;
     SurfaceData* most_recent_popup; // Start of the popup linked list
     SurfaceData* previous_popup_sibling; // Forms a linked list of popups
     SurfaceData* popup_parent;
@@ -34,17 +38,33 @@ struct SurfaceData {
 static struct wl_resource* seat_global = NULL;
 static struct wl_resource* pointer_global = NULL;
 static struct wl_resource* output_global = NULL;
+static struct wl_resource* current_session_lock = NULL;
+
+static void surface_data_assert_no_role(SurfaceData* data) {
+    ASSERT(!data->xdg_popup);
+    ASSERT(!data->xdg_toplevel);
+    ASSERT(!data->xdg_surface);
+    ASSERT(!data->layer_surface);
+    ASSERT(!data->lock_surface);
+}
 
 // Needs to be called before any role objects are assigned
 static void surface_data_set_role(SurfaceData* data, SurfaceRole role) {
     if (data->role != SURFACE_ROLE_NONE) {
         ASSERT_EQ(data->role, role, "%u");
     }
-    char is_xdg_role = (role == SURFACE_ROLE_XDG_TOPLEVEL || role == SURFACE_ROLE_XDG_POPUP);
-    ASSERT_EQ(data->xdg_surface != NULL, is_xdg_role, "%d");
-    ASSERT(!data->xdg_toplevel);
-    ASSERT(!data->xdg_popup);
-    ASSERT(!data->layer_surface);
+
+    if (role == SURFACE_ROLE_XDG_TOPLEVEL || role == SURFACE_ROLE_XDG_POPUP) {
+        ASSERT(data->xdg_surface != NULL);
+    } else {
+        ASSERT(data->xdg_surface == NULL);
+    }
+
+    struct wl_resource* xdg_surface = data->xdg_surface;
+    data->xdg_surface = NULL; // XDG surfaces are allowed, so hide it from surface_data_assert_no_role()
+    surface_data_assert_no_role(data);
+    data->xdg_surface = xdg_surface;
+
     ASSERT(!data->has_committed_buffer);
     data->role = role;
     data->initial_commit_for_role = 1;
@@ -54,10 +74,7 @@ static void surface_data_unmap(SurfaceData* data) {
     SurfaceData* popup = data->most_recent_popup;
     while (popup) {
         // Popups must be unmapped before their parents
-        ASSERT(!popup->layer_surface);
-        ASSERT(!popup->xdg_popup);
-        ASSERT(!popup->xdg_toplevel);
-        ASSERT(!popup->xdg_surface);
+        surface_data_assert_no_role(data);
         popup = popup->previous_popup_sibling;
     }
 }
@@ -89,11 +106,21 @@ static void wl_surface_attach(struct wl_resource *resource, const struct wl_mess
 
 static void wl_surface_commit(struct wl_resource *resource, const struct wl_message* message, union wl_argument* args) {
     SurfaceData* data = wl_resource_get_user_data(resource);
+
+    if (data->role == SURFACE_ROLE_SESSION_LOCK) {
+        if (data->buffer_cleared) {
+            FATAL("null buffer committed to session lock surface");
+        } else if (!data->pending_buffer && !data->has_committed_buffer) {
+            FATAL("no buffer has been attached to committed session lock surface");
+        } else if (!data->lock_surface_initial_configure_acked) {
+            FATAL("session lock surface committed before initial configure acked");
+        }
+    }
+
     if (data->buffer_cleared) {
         data->has_committed_buffer = 0;
         data->buffer_cleared = 0;
-    }
-    else if (data->pending_buffer) {
+    } else if (data->pending_buffer) {
         data->has_committed_buffer = 1;
     }
 
@@ -108,7 +135,7 @@ static void wl_surface_commit(struct wl_resource *resource, const struct wl_mess
         data->pending_frame = NULL;
     }
 
-    if (data->initial_commit_for_role) {
+    if (data->initial_commit_for_role && data->role != SURFACE_ROLE_SESSION_LOCK) {
         ASSERT(!data->has_committed_buffer);
         data->initial_commit_for_role = 0;
     }
@@ -137,10 +164,7 @@ static void wl_surface_commit(struct wl_resource *resource, const struct wl_mess
 
 static void wl_surface_destroy(struct wl_resource* resource, const struct wl_message* message, union wl_argument* args) {
     SurfaceData* data = wl_resource_get_user_data(resource);
-    ASSERT(!data->xdg_popup);
-    ASSERT(!data->xdg_toplevel);
-    ASSERT(!data->xdg_surface);
-    ASSERT(!data->layer_surface);
+    surface_data_assert_no_role(data);
     data->surface = NULL;
     // Don't free surfaces to guarantee traversing popups is always safe
     // We're employing the missile memory management pattern here https://x.com/pomeranian99/status/858856994438094848
@@ -314,6 +338,66 @@ static void zwlr_layer_surface_v1_destroy(struct wl_resource *resource, const st
     surface_data_unmap(data);
 }
 
+static void ext_session_lock_manager_v1_lock(struct wl_resource *resource, const struct wl_message* message, union wl_argument* args) {
+    NEW_ID_ARG(id, 0);
+    struct wl_resource* lock_resource = wl_resource_create(
+        wl_resource_get_client(resource),
+        &ext_session_lock_v1_interface,
+        wl_resource_get_version(resource),
+        id);
+    use_default_impl(lock_resource);
+    if (current_session_lock) {
+        ext_session_lock_v1_send_finished(lock_resource);
+    } else {
+        current_session_lock = lock_resource;
+        ext_session_lock_v1_send_locked(lock_resource);
+    }
+}
+
+static void ext_session_lock_v1_destroy(struct wl_resource *resource, const struct wl_message* message, union wl_argument* args) {
+    if (resource == current_session_lock) {
+        FATAL(".destroy (instead of .unlock_and_destroy) called on active lock");
+    }
+}
+
+static void ext_session_lock_v1_unlock_and_destroy(struct wl_resource *resource, const struct wl_message* message, union wl_argument* args) {
+    if (resource != current_session_lock) {
+        FATAL(".unlock_and_destroy (instead of .destroy) called on inactive lock");
+    }
+    current_session_lock = NULL;
+}
+
+static void lock_surface_send_configure(SurfaceData* data, uint32_t width, uint32_t height) {
+    data->lock_surface_pending_serial = wl_display_next_serial(display);
+    ext_session_lock_surface_v1_send_configure(data->lock_surface, data->lock_surface_pending_serial, width, height);
+}
+
+static void ext_session_lock_v1_get_lock_surface(struct wl_resource *resource, const struct wl_message* message, union wl_argument* args) {
+    NEW_ID_ARG(id, 0);
+    struct wl_resource* lock_surface = wl_resource_create(
+        wl_resource_get_client(resource),
+        &ext_session_lock_surface_v1_interface,
+        wl_resource_get_version(resource),
+        id);
+    use_default_impl(lock_surface);
+    RESOURCE_ARG(wl_surface, surface, 1);
+    RESOURCE_ARG(wl_output, output, 2);
+    ASSERT_EQ(output, output_global, "%p");
+    SurfaceData* data = wl_resource_get_user_data(surface);
+    surface_data_set_role(data, SURFACE_ROLE_SESSION_LOCK);
+    wl_resource_set_user_data(lock_surface, data);
+    data->lock_surface = lock_surface;
+    lock_surface_send_configure(data, DEFAULT_OUTPUT_WIDTH, DEFAULT_OUTPUT_HEIGHT);
+}
+
+static void ext_session_lock_surface_v1_ack_configure(struct wl_resource *resource, const struct wl_message* message, union wl_argument* args) {
+    UINT_ARG(serial, 0);
+    SurfaceData* data = wl_resource_get_user_data(resource);
+    if (serial == data->lock_surface_pending_serial) {
+        data->lock_surface_initial_configure_acked = 1;
+    }
+}
+
 void init() {
     OVERRIDE_REQUEST(wl_surface, commit);
     OVERRIDE_REQUEST(wl_surface, frame);
@@ -333,6 +417,11 @@ void init() {
     OVERRIDE_REQUEST(zwlr_layer_surface_v1, set_size);
     OVERRIDE_REQUEST(zwlr_layer_surface_v1, get_popup);
     OVERRIDE_REQUEST(zwlr_layer_surface_v1, destroy);
+    OVERRIDE_REQUEST(ext_session_lock_manager_v1, lock);
+    OVERRIDE_REQUEST(ext_session_lock_v1, destroy);
+    OVERRIDE_REQUEST(ext_session_lock_v1, unlock_and_destroy);
+    OVERRIDE_REQUEST(ext_session_lock_v1, get_lock_surface);
+    OVERRIDE_REQUEST(ext_session_lock_surface_v1, ack_configure);
 
     wl_global_create(display, &wl_seat_interface, 6, NULL, wl_seat_bind);
     wl_global_create(display, &wl_output_interface, 2, NULL, wl_output_bind);
@@ -342,5 +431,6 @@ void init() {
     default_global_create(display, &wl_subcompositor_interface, 1);
     default_global_create(display, &xdg_wm_base_interface, 2);
     default_global_create(display, &zwlr_layer_shell_v1_interface, 4);
+    default_global_create(display, &ext_session_lock_manager_v1_interface, 1);
     default_global_create(display, &xdg_wm_dialog_v1_interface, 1);
 }
