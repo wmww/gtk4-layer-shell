6add453de44373d1aa23018f714d5842091033d5
Sophie Winter <git@phie.me>
2025-03-11 22:48:21 -0700

    Mock server: simplify override implementation

--- test/mock-server/mock-server.h
+++ test/mock-server/mock-server.h
@@ -20,12 +20,16 @@ extern struct wl_display* display;
 #define ALLOC_STRUCT(type) ((type*)alloc_zeroed(sizeof(type)))
 void* alloc_zeroed(size_t size);
 
+#define REQUEST_OVERRIDE_IMPL(type, method) static void type##_##method( \
+    struct wl_resource* type, \
+    const struct wl_message* message, \
+    struct wl_resource* new_resource, \
+    union wl_argument* args)
 #define OVERRIDE_REQUEST(type, method) install_request_override(&type##_interface, #method, type##_##method)
-#define NEW_ID_ARG(name, index) ASSERT(type_code_at_index(message, index) == 'n'); uint32_t name = args[index].n;
 #define RESOURCE_ARG(type, name, index) ASSERT(type_code_at_index(message, index) == 'o'); ASSERT(message->types[index] == &type##_interface); struct wl_resource* name = (struct wl_resource*)args[index].o;
 #define UINT_ARG(name, index) ASSERT(type_code_at_index(message, index) == 'u'); uint32_t name = args[index].u;
 
-typedef void (*RequestOverrideFunction)(struct wl_resource* resource, const struct wl_message* message, union wl_argument* args);
+typedef void (*RequestOverrideFunction)(struct wl_resource* resource, const struct wl_message* message, struct wl_resource* created, union wl_argument* args);
 void install_request_override(const struct wl_interface* interface, const char* name, RequestOverrideFunction function);
 void use_default_impl(struct wl_resource* resource);
 void default_global_create(struct wl_display* display, const struct wl_interface* interface, int version);
