61f31928cceafcc174feda457fca8adf279c4a67
Sophie Winter <git@phie.me>
2025-03-25 02:29:15 -0700

    Add hacky support for running tests under Sway

--- test/layer-tests/test-uses-widget-size.c
+++ test/layer-tests/test-uses-widget-size.c
@@ -4,7 +4,7 @@ static GtkWindow* window;
 static GtkWidget* child;
 
 static void callback_0() {
-    EXPECT_MESSAGE(.create_buffer 850 940);
+    EXPECT_MESSAGE(new id wl_buffer 850 940);
     EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_size 850 940);
 
     window = create_default_window();
@@ -16,7 +16,7 @@ static void callback_0() {
 }
 
 static void callback_1() {
-    EXPECT_MESSAGE(.create_buffer 1001 610);
+    EXPECT_MESSAGE(new id wl_buffer 1001 610);
     EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_size 1001 610);
 
     gtk_widget_set_size_request(child, 1001, 610);
@@ -24,7 +24,7 @@ static void callback_1() {
 
 static void callback_2() {
     EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_size 1001 0);
-    EXPECT_MESSAGE(.create_buffer 1001 1080); // size must match DEFAULT_OUTPUT_HEIGHT in common.h
+    EXPECT_MESSAGE(new id wl_buffer 1001 1080); // size must match DEFAULT_OUTPUT_HEIGHT in common.h
 
     gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_TOP, TRUE);
     gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
@@ -32,14 +32,14 @@ static void callback_2() {
 
 static void callback_3() {
     EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_size 0 0);
-    EXPECT_MESSAGE(.create_buffer 1920 1080);
+    EXPECT_MESSAGE(new id wl_buffer 1920 1080);
 
     gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_LEFT, TRUE);
     gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
 }
 
 static void callback_4() {
-    EXPECT_MESSAGE(.create_buffer 555 777);
+    EXPECT_MESSAGE(new id wl_buffer 555 777);
     EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_size 555 777);
 
     gtk_widget_set_size_request(child, 555, 777);
