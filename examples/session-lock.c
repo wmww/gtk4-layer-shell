5465bc520c5030281d6693d49f7e797b3bde59d0
Sophie Winter <git@phie.me>
2025-08-07 20:55:57 -0700

    Session lock example: allow locking multiple times

--- examples/session-lock.c
+++ examples/session-lock.c
@@ -6,22 +6,18 @@ static GtkApplication* app = NULL;
 static void unlock(GtkButton *button, void *data) {
     (void)button;
     GtkSessionLockInstance* lock = data;
-
     gtk_session_lock_instance_unlock(lock);
-    g_application_quit(G_APPLICATION(app));
 }
 
 static void locked(GtkSessionLockInstance *lock, void *data) {
     (void)lock;
     (void)data;
-
     g_message("Session locked successfully");
 }
 
 static void failed(GtkSessionLockInstance *lock, void *data) {
     (void)lock;
     (void)data;
-
     g_critical("The session could not be locked");
     g_application_quit(G_APPLICATION(app));
 }
@@ -29,9 +25,7 @@ static void failed(GtkSessionLockInstance *lock, void *data) {
 static void unlocked(GtkSessionLockInstance *lock, void *data) {
     (void)lock;
     (void)data;
-
     g_message("Session unlocked");
-    g_application_quit(G_APPLICATION(app));
 }
 
 static void create_lock_surface(GtkSessionLockInstance* lock, GdkMonitor *monitor) {
@@ -41,7 +35,7 @@ static void create_lock_surface(GtkSessionLockInstance* lock, GdkMonitor *monito
     GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
-    gtk_box_set_spacing(GTK_BOX (box), 10);
+    gtk_box_set_spacing(GTK_BOX(box), 10);
 
     GtkWidget *label = gtk_label_new("GTK Session Lock example");
     gtk_box_append(GTK_BOX(box), label);
@@ -68,19 +62,62 @@ static void create_lock_surface_for_all_monitors(GtkSessionLockInstance* lock) {
     }
 }
 
+static void lock_display(GtkSessionLockInstance* lock) {
+    if (gtk_session_lock_instance_lock(lock)) {
+        create_lock_surface_for_all_monitors(lock);
+    } else {
+        // Error message already shown when handling the ::failed signal
+    }
+}
+
+static void lock_button_callback(GtkWidget* button, void* data) {
+    (void)button;
+    lock_display(data);
+}
+
+static void quit_button_callback(GtkWidget* button, void* data) {
+    (void)button;
+    GtkSessionLockInstance* lock = data;
+    if (gtk_session_lock_instance_is_locked(lock)) {
+        g_critical("Quit button somehow pressed while session lock was locked??");
+        // If we quit now there would be no way for the user to unlock, don't do that
+        return;
+    }
+    g_message("Quitting");
+    g_application_quit(G_APPLICATION(app));
+}
+
+// Creates a normal GTK window with buttons to allow the user to re-lock the display or exit
+static void create_control_window(GtkSessionLockInstance* lock) {
+    GtkWidget* lock_button = gtk_button_new_with_label("Lock");
+    g_signal_connect(lock_button, "clicked", G_CALLBACK(lock_button_callback), lock);
+
+    GtkWidget* quit_button = gtk_button_new_with_label("Quit");
+    g_signal_connect(quit_button, "clicked", G_CALLBACK(quit_button_callback), lock);
+
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    gtk_widget_set_valign(box, GTK_ALIGN_START);
+    gtk_box_append(GTK_BOX(box), lock_button);
+    gtk_box_append(GTK_BOX(box), quit_button);
+
+    GtkWindow* control_window = GTK_WINDOW(gtk_application_window_new(app));
+    gtk_window_set_child(control_window, box);
+    gtk_window_present(control_window);
+}
+
 static void activate(GtkApplication* app, void *data) {
     (void)data;
     g_application_hold(G_APPLICATION(app));
 
+    // This creates the lock instance, but does not lock the display yet
     GtkSessionLockInstance* lock = gtk_session_lock_instance_new();
     g_signal_connect(lock, "locked", G_CALLBACK(locked), NULL);
     g_signal_connect(lock, "failed", G_CALLBACK(failed), NULL);
     g_signal_connect(lock, "unlocked", G_CALLBACK(unlocked), NULL);
 
-    if (gtk_session_lock_instance_lock(lock)) {
-        create_lock_surface_for_all_monitors(lock);
-    }
-    // Else error message already shown when handling the ::failed signal
+    create_control_window(lock);
+
+    lock_display(lock);
 }
 
 int main(int argc, char **argv) {
