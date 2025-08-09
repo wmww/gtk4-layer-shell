183b2b33965ec239657280b1d0d489d60be7033e
Sophie Winter <git@phie.me>
2025-08-09 06:13:53 -0700

    Add session lock multi monitor tests

---
+++ test/lock-tests/test-multiple-monitors.c
@@ -0,0 +1,39 @@
+#include "integration-test-common.h"
+
+enum lock_state_t state = 0;
+GtkSessionLockInstance* lock;
+
+static void callback_0() {
+    EXPECT_MESSAGE(ext_session_lock_manager_v1 .lock);
+    EXPECT_MESSAGE(ext_session_lock_v1 .get_lock_surface);
+    EXPECT_MESSAGE(ext_session_lock_surface_v1 .configure 1920 1080);
+    EXPECT_MESSAGE(ext_session_lock_v1 .get_lock_surface);
+    EXPECT_MESSAGE(ext_session_lock_surface_v1 .configure 640 480);
+    EXPECT_MESSAGE(ext_session_lock_v1 .get_lock_surface);
+    EXPECT_MESSAGE(ext_session_lock_surface_v1 .configure 1080 720);
+
+    send_command("create_output 640 480", "output_created");
+    send_command("create_output 1080 720", "output_created");
+
+    lock = gtk_session_lock_instance_new();
+    connect_lock_signals(lock, &state);
+
+    ASSERT(gtk_session_lock_instance_lock(lock));
+}
+
+static void callback_1() {
+    ASSERT_EQ(state, LOCK_STATE_LOCKED, "%d");
+    EXPECT_MESSAGE(ext_session_lock_v1 .unlock_and_destroy);
+
+    gtk_session_lock_instance_unlock(lock);
+}
+
+static void callback_2() {
+    ASSERT_EQ(state, LOCK_STATE_UNLOCKED, "%d");
+}
+
+TEST_CALLBACKS(
+    callback_0,
+    callback_1,
+    callback_2,
+)
