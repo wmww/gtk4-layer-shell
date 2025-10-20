160f75ea4f4f0d8656b73a3803b0187a9d3d3e90
Sophie Winter <git@phie.me>
2025-10-19 22:28:53 -0700

    Test respect close behavior

---
+++ test/layer-tests/test-respect-close.c
@@ -0,0 +1,32 @@
+#include "integration-test-common.h"
+
+static GtkWindow* window;
+
+static void callback_0()
+{
+    EXPECT_MESSAGE(zwlr_layer_shell_v1 .get_layer_surface nil);
+    window = create_default_window();
+    gtk_layer_init_for_window(window);
+    gtk_layer_set_respect_close(window, TRUE);
+    ASSERT_EQ(g_list_model_get_n_items(gdk_display_get_monitors(gdk_display_get_default())), 1, "%d");
+    gtk_window_present(window);
+}
+
+static void callback_1()
+{
+    EXPECT_MESSAGE(zwlr_layer_surface_v1 .closed);
+    EXPECT_MESSAGE(wl_surface .destroy);
+    send_command("destroy_output 0", "output_destroyed");
+}
+
+static void callback_2()
+{
+    // Because we set respect_close to TRUE the window should have been automatically unmapped from the .closed event
+    ASSERT(!gtk_widget_get_mapped(GTK_WIDGET(window)));
+}
+
+TEST_CALLBACKS(
+    callback_0,
+    callback_1,
+    callback_2,
+)
