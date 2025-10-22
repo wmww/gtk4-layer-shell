3120be8751de5cfc37f2de4ea009045528d7c540
Sophie Winter <git@phie.me>
2025-10-21 20:34:10 -0700

    Fix use-after-free in layer-test-respect-close

--- test/layer-tests/test-respect-close.c
+++ test/layer-tests/test-respect-close.c
@@ -5,7 +5,7 @@ static GtkWindow* window;
 static void callback_0()
 {
     EXPECT_MESSAGE(zwlr_layer_shell_v1 .get_layer_surface nil);
-    window = create_default_window();
+    window = g_object_ref(create_default_window());
     gtk_layer_init_for_window(window);
     gtk_layer_set_respect_close(window, TRUE);
     ASSERT_EQ(g_list_model_get_n_items(gdk_display_get_monitors(gdk_display_get_default())), 1, "%d");
@@ -23,6 +23,8 @@ static void callback_2()
 {
     // Because we set respect_close to TRUE the window should have been automatically unmapped from the .closed event
     ASSERT(!gtk_widget_get_mapped(GTK_WIDGET(window)));
+    ASSERT_EQ(G_OBJECT(window)->ref_count, 1, "%d");
+    g_object_unref(window);
 }
 
 TEST_CALLBACKS(
